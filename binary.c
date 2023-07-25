6ff77003f90a0a0a1d2641317db1458af373f715
Ayobami <tnenas07@gmail.com>
2023-07-26 00:15:21 +0100

    printf

---
+++ binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_binary - Print a number in the base 2
+ * @list: Number to be printed in base 2
+ *
+ * Return: Length of numbers in binary
+ **/
+int print_binary(va_list list)
+{
+	char *p_buff;
+	int size;
+
+	p_buff = itoa(va_arg(list, unsigned int), 2);
+
+	size = print(p_buff);
+
+	return (size);
+}
