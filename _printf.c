4723cdf78c1674c84eec12d38ba345bb48d98f8d
Sucexpee <133268315+Sucexpee@users.noreply.github.com>
2023-07-26 00:21:52 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,7 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format string.
- * Return: number of characters printed.
+ * Return: The number of characters printed.
  */
 int _printf(const char *format, ...)
 {
